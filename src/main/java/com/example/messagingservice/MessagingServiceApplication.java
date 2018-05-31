62f0f404dccc117e686b8da82455855357a7abf8
Lalitanand Dandge <dandgelb@gmail.com>
2018-06-01 03:38:36 +0530

    Add connections relationship
    FIXME:
    (2, [1]) is not possible after adding (1, [2, 3])
    
    Update configuration to access H2 Console - 'http://localhost:8080/h2-console/'.
    Connection properties =>
    JDBC URL : 'jdbc:h2:mem:testdb'
    User Name: 'sa'
    Password : ''

--- src/main/java/com/example/messagingservice/MessagingServiceApplication.java
+++ src/main/java/com/example/messagingservice/MessagingServiceApplication.java
@@ -10,9 +10,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.stereotype.Component;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
 import java.util.stream.LongStream;
 
 @SpringBootApplication
@@ -33,8 +33,21 @@ class SampleUserDataCLR implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
+        //Add users
         LongStream.of(1, 2, 3, 4, 5)
         .forEach(userId -> userRepository.save(new User(userId)));
+
+        //Add connections
+        HashSet set = new HashSet<>();
+        LongStream.of(2, 3)
+                .forEach(connectionId -> set.add(new User(connectionId)));
+        userRepository.save(new User(new Long(1), set));
+
+        HashSet s2 = new HashSet<>();
+        s2.add(new User(new Long(2)));
+        userRepository.save(new User(new Long(3), s2));
+
+        //print
         userRepository.findAll().forEach(System.out::println);
     }
 }
@@ -52,4 +65,12 @@ class User {
     @Id
     @GeneratedValue
     private Long id;
+
+    @JoinTable(name="connections")
+    @ManyToMany(fetch = FetchType.EAGER)
+    Set<User> connection;
+
+    User(Long userId) {
+        id = userId;
+    }
 }
