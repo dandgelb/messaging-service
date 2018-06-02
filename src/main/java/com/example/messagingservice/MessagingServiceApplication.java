d04d834bc874b4142cf75c880bc0d68ea3479ef0
Lalitanand Dandge <dandgelb@gmail.com>
2018-06-02 18:58:27 +0530

    Add Repository and RestController for user connections
    TODO:
    Explore RestResource as a response to /users/{id}/connections

--- src/main/java/com/example/messagingservice/MessagingServiceApplication.java
+++ src/main/java/com/example/messagingservice/MessagingServiceApplication.java
@@ -3,15 +3,24 @@ package com.example.messagingservice;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.persistence.*;
 import java.io.Serializable;
+import java.util.List;
 import java.util.stream.LongStream;
 
 @SpringBootApplication
@@ -36,6 +45,24 @@ class SampleUserDataCLR implements CommandLineRunner {
         .forEach(userId -> userRepository.save(new User(userId)));
         userRepository.findAll().forEach(System.out::println);
     }
+}
+@RestController
+class ConnectionController {
+    @Autowired
+    ConnectionRepository connectionRepository;
+
+    @GetMapping("/users/{id}/connections")
+    public ResponseEntity<List<User>> getConnectionsByUserId(@PathVariable(value = "id") Long userId) {
+        List<User> connections = connectionRepository.findConnectionsByUserId(userId);
+        return new ResponseEntity<List<User>>(connections, HttpStatus.OK);
+    }
+}
+
+@Repository
+interface ConnectionRepository extends JpaRepository<Connection, Long> {
+    @Query("select c.id.connection from Connection c where c.id.user.id = ?1")
+    List<User> findConnectionsByUserId(Long userId);
+
 }
 
 @RepositoryRestResource
