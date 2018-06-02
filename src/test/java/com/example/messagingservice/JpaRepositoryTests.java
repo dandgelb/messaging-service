68ea5713da76bddb1627cb42e63c48ecbc771f21
Lalitanand Dandge <dandgelb@gmail.com>
2018-06-02 23:14:25 +0530

    Separate user seed creation

--- src/test/java/com/example/messagingservice/JpaRepositoryTests.java
+++ src/test/java/com/example/messagingservice/JpaRepositoryTests.java
@@ -9,8 +9,10 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 import javax.persistence.EntityExistsException;
 import javax.persistence.PersistenceException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.NoSuchElementException;
+import java.util.stream.LongStream;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
@@ -26,6 +28,13 @@ public class JpaRepositoryTests {
     @Autowired
     private ConnectionRepository connectionRepository;
 
+    public List<User> userEntitySeeder(){
+        List<User> userList = new ArrayList<>();
+        LongStream.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+                .forEach(userId -> userList.add(new User(Long.valueOf(userId))));
+        return userList;
+    }
+
     @Test
     public void testUserCreateAndReadOperations() {
         //given - no users
@@ -36,8 +45,7 @@ public class JpaRepositoryTests {
         assertThat(total).isEqualTo(0);
 
         //given - one user created
-        User user = new User(new Long(1));
-        entityManager.persist(user);
+        entityManager.persist(userEntitySeeder().get(0));
         entityManager.flush();
         //when  - findOne
         User found = userRepository.findById(new Long(1)).get();
@@ -50,24 +58,25 @@ public class JpaRepositoryTests {
     @Test(expected = NoSuchElementException.class)
     public void testUserDeleteOperation() {
         //given - two users created
-        User u2 = new User(Long.valueOf(2));
-        User u3 = new User(Long.valueOf(3));
-        entityManager.persist(u2);
-        entityManager.persist(u3);
+        List<User> users = userEntitySeeder();
+        entityManager.persist(users.get(0));
+        entityManager.persist(users.get(1));
         //when  - one user deleted
-        userRepository.delete(u3);
-        User u2Found = userRepository.findById(Long.valueOf(3)).get();
+        userRepository.delete(users.get(1));
+        User u2Found = userRepository.findById(Long.valueOf(2)).get();
     }
 
     @Test(expected = PersistenceException.class)
     public void testConnectionCreationWithoutUserCreated() {
-        Connection connection = new Connection(new ConnectionId(new User(Long.valueOf(1)), new User(Long.valueOf(2))));
+        List<User> users = userEntitySeeder();
+        Connection connection = new Connection(new ConnectionId(users.get(0), users.get(1)));
         entityManager.persist(connection);
         entityManager.flush();
     }
 
     @Test
     public void testConnectionCreateAndRead() {
+        List<User> users = userEntitySeeder();
         //given - no connections
 
         //when  - tried to findAll
@@ -76,15 +85,12 @@ public class JpaRepositoryTests {
         assertThat(total).isEqualTo(0);
 
         //given - at least two users created - connection could be created
-        User u1 = new User(Long.valueOf(1));
-        User u2 = new User(Long.valueOf(2));
-        User u3 = new User(Long.valueOf(3));
-        entityManager.persist(u1);
-        entityManager.persist(u2);
-        entityManager.persist(u3);
+        entityManager.persist(users.get(0));
+        entityManager.persist(users.get(1));
+        entityManager.persist(users.get(2));
 
-        ConnectionId ci12 = new ConnectionId(u1, u2);
-        ConnectionId ci13 = new ConnectionId(u1, u3);
+        ConnectionId ci12 = new ConnectionId(users.get(0), users.get(1));
+        ConnectionId ci13 = new ConnectionId(users.get(0), users.get(2));
 
         Connection c1 = new Connection(ci12);
         Connection c2 = new Connection(ci13);
@@ -95,18 +101,19 @@ public class JpaRepositoryTests {
         List<User> found = connectionRepository.findConnectionsByUserId(Long.valueOf(1));
         //then  - it is the same user
         assertThat(found.size()).isEqualTo(2);
-        assertThat(found.get(0).getId()). isEqualTo(u2.getId());
-        assertThat(found.get(1).getId()). isEqualTo(u3.getId());
+        assertThat(found.get(0).getId()). isEqualTo(users.get(1).getId());
+        assertThat(found.get(1).getId()). isEqualTo(users.get(2).getId());
     }
 
     @Test(expected = EntityExistsException.class)
     public void testDuplicateConnectionCreation() {
+        List<User> users = userEntitySeeder();
         //given - at two users created - connection could be created
-        entityManager.persist(new User(Long.valueOf(1)));
-        entityManager.persist(new User(Long.valueOf(2)));
+        entityManager.persist(users.get(0));
+        entityManager.persist(users.get(1));
 
-        Connection c1 = new Connection(new ConnectionId(new User(Long.valueOf(1)), new User(Long.valueOf(2))));
-        Connection c2 = new Connection(new ConnectionId(new User(Long.valueOf(1)), new User(Long.valueOf(2))));
+        Connection c1 = new Connection(new ConnectionId(users.get(0), users.get(1)));
+        Connection c2 = new Connection(new ConnectionId(users.get(0), users.get(1)));
         entityManager.persist(c1);
         entityManager.flush();
         entityManager.persist(c2);
@@ -116,12 +123,13 @@ public class JpaRepositoryTests {
     @Test
     public void testMultipleConnectionForUser() {
         //given - at two users created - connection could be created
-        entityManager.persist(new User(Long.valueOf(1)));
-        entityManager.persist(new User(Long.valueOf(2)));
-        entityManager.persist(new User(Long.valueOf(3)));
+        List<User> users = userEntitySeeder();
+        entityManager.persist(users.get(0));
+        entityManager.persist(users.get(1));
+        entityManager.persist(users.get(2));
 
-        Connection c1 = new Connection(new ConnectionId(new User(Long.valueOf(1)), new User(Long.valueOf(2))));
-        Connection c2 = new Connection(new ConnectionId(new User(Long.valueOf(1)), new User(Long.valueOf(3))));
+        Connection c1 = new Connection(new ConnectionId(users.get(0), users.get(1)));
+        Connection c2 = new Connection(new ConnectionId(users.get(0), users.get(2)));
         entityManager.persist(c1);
         entityManager.flush();
         entityManager.persist(c2);
@@ -134,15 +142,13 @@ public class JpaRepositoryTests {
     @Test
     public void testConnectionDelete() {
         //given - at two users created - connection could be created
-        User u1 = new User(Long.valueOf(1));
-        User u2 = new User(Long.valueOf(2));
-        User u3 = new User(Long.valueOf(3));
-        entityManager.persist(u1);
-        entityManager.persist(u2);
-        entityManager.persist(u3);
+        List<User> users = userEntitySeeder();
+        entityManager.persist(users.get(0));
+        entityManager.persist(users.get(1));
+        entityManager.persist(users.get(2));
 
-        ConnectionId ci12 = new ConnectionId(u1, u2);
-        ConnectionId ci13 = new ConnectionId(u1, u3);
+        ConnectionId ci12 = new ConnectionId(users.get(0), users.get(1));
+        ConnectionId ci13 = new ConnectionId(users.get(0), users.get(2));
 
         Connection c1 = new Connection(ci12);
         Connection c2 = new Connection(ci13);
@@ -155,7 +161,7 @@ public class JpaRepositoryTests {
         List<User> found = connectionRepository.findConnectionsByUserId(Long.valueOf(1));
         //then  - it is the same user
         assertThat(found.size()).isEqualTo(1);
-        assertThat(found.get(0).getId()). isEqualTo(u3.getId());
+        assertThat(found.get(0).getId()). isEqualTo(users.get(2).getId());
         //TODO: Think better way of unit testing this
     }
 
